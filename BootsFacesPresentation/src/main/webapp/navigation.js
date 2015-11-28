107cd7277605e4d0bbfe9ced24ebd746f8c7d8bc
stephan <webmaster1@beyondjava.de>
2015-11-29 00:21:49 +0100

    Implemented basic navigation

--- BootsFacesPresentation/src/main/webapp/navigation.js
+++ BootsFacesPresentation/src/main/webapp/navigation.js
@@ -10,6 +10,15 @@ navigationApp.config([ '$routeProvider', function($routeProvider) {
 	}).when('/', {
 		templateUrl : '1_intro/start.jsf',
 		controller : 'EmptyController'
+	}).when('/start', {
+		templateUrl : '1_intro/start.jsf',
+		controller : 'EmptyController'
+	}).when('/start2', {
+		templateUrl : '1_intro/start2.jsf',
+		controller : 'EmptyController'
+	}).when('/gridSystem', {
+		templateUrl : '2_layout/gridSystem.jsf',
+		controller : 'EmptyController'
 	}).otherwise({
 	  templateUrl : '99_miscellaneous/other.jsf',
 	});
