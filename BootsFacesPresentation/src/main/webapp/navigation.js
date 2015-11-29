f60cc8bea24e1415c8ece4c0957ae7f9d3d390ae
stephan <webmaster1@beyondjava.de>
2015-11-29 21:48:23 +0100

    added an AngularJS navigation component (inspired by reveal.js)

--- BootsFacesPresentation/src/main/webapp/navigation.js
+++ BootsFacesPresentation/src/main/webapp/navigation.js
@@ -26,6 +26,53 @@ navigationApp.config([ '$routeProvider', function($routeProvider) {
 	}).when('/angularJS', {
 		templateUrl : '10_angularJS/angularJS.jsf',
 	}).otherwise({
-	  templateUrl : '99_miscellaneous/other.jsf',
+		templateUrl : '99_miscellaneous/other.jsf',
 	});
 } ]);
+
+navigationApp.directive('navigator', function() {
+	return {
+		restrict : 'E',
+		scope : {},
+		template : function($scope, $element) {
+
+			var msg = '<aside class="controls">';
+			msg += '<div class="navigate-left';
+			if ($element['left']) {
+				msg += ' enabled" onclick="window.location.href=\''
+						+ $element['left'] + '\'";';
+			} else
+				msg += '"';
+			msg += '></div>';
+			msg += '<div class="navigate-right';
+			if ($element['right']) {
+				msg += ' enabled" onclick="window.location.href=\''
+						+ $element['right'] + '\'";';
+			} else
+				msg += '"';
+			msg += '></div>';
+			msg += '<div class="navigate-up';
+			if ($element['up']) {
+				msg += ' enabled" onclick="window.location.href=\''
+						+ $element['up'] + '\'";';
+			} else
+				msg += '"';
+			msg += '></div>';
+			msg += '<div class="navigate-down';
+			if ($element['down']) {
+				msg += ' enabled" onclick="window.location.href=\''
+						+ $element['down'] + '\'";';
+			} else
+				msg += '"';
+			msg += '></div>';
+			msg += '</aside>';
+
+
+			return msg;
+		},
+		controller : function($scope) {
+			// $scope.currentMessage="";
+		},
+
+	};
+});
