ab1c84582a89ec9e3a8770f6de2bb48b2176febc
stephan <webmaster1@beyondjava.de>
2015-11-29 12:53:22 +0100

    added slides

--- BootsFacesPresentation/src/main/webapp/navigation.js
+++ BootsFacesPresentation/src/main/webapp/navigation.js
@@ -16,9 +16,15 @@ navigationApp.config([ '$routeProvider', function($routeProvider) {
 	}).when('/start2', {
 		templateUrl : '1_intro/start2.jsf',
 		controller : 'EmptyController'
+	}).when('/keyFeaturesBF', {
+		templateUrl : '1_intro/keyFeaturesBF.jsf',
+	}).when('/keyFeaturesAF', {
+		templateUrl : '1_intro/keyFeaturesAF.jsf',
 	}).when('/gridSystem', {
 		templateUrl : '2_layout/gridSystem.jsf',
 		controller : 'EmptyController'
+	}).when('/angularJS', {
+		templateUrl : '10_angularJS/angularJS.jsf',
 	}).otherwise({
 	  templateUrl : '99_miscellaneous/other.jsf',
 	});
